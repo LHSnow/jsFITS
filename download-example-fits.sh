7200ed00a12c1a6cc9544479ac64cba9d06092c5
Lars-Henrik Snow <lars-henrik.snow@irf.se>
2022-09-05 08:41:23 +0200

    ci: Replacing curl with wget to avoid install

--- download-example-fits.sh
+++ download-example-fits.sh
@@ -1,4 +1,4 @@
 mkdir -p public/files
 while read filename; do
-  curl "http://dalek.umea.irf.se/peje/shrink-fits/2022-03-03/19/$filename" -o public/files/$filename
+  wget -q "http://dalek.umea.irf.se/peje/shrink-fits/2022-03-03/19/$filename" -O public/files/$filename
 done < example-fits-files.txt
