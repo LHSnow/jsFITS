327f0e9ae311c8201913a08db9ff21b1751b974a
Lars-Henrik Snow <lars-henrik.snow@irf.se>
2022-12-20 11:11:39 +0100

    fix: cut the right number of fields (including first /)

--- find-alis-cubes.sh
+++ find-alis-cubes.sh
@@ -4,7 +4,7 @@ day=$(date -d "$radix" '+%Y/%m/%d')
 day_before=$(date -d "$radix -1 day" '+%Y/%m/%d')
 
 find "$path/$day" "$path/$day_before" -name *.cube.fits 2>/dev/null \
-| cut -d'/' -f-5 | uniq | while read day
+| cut -d'/' -f1-6 | uniq | while read d
 do
-  find "$day" -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$day/cube.json";
+  find "$d" -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$d/cube.json";
 done
