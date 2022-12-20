a896e86e7e9fc070ffea160cf0cd764d7082a47f
Lars-Henrik Snow <lars-henrik.snow@irf.se>
2022-12-20 11:03:06 +0100

    accept day (supported by linux date()) as input to find

--- find-alis-cubes.sh
+++ find-alis-cubes.sh
@@ -1,8 +1,9 @@
-start='/alis/data'
-today=$(date '+%Y/%m/%d')
-yesterday=$(date -d '1 day ago' '+%Y/%m/%d')
+path='/alis/data'
+radix=${1:-today}
+day=$(date -d "$radix" '+%Y/%m/%d')
+day_before=$(date -d "$radix -1 day" '+%Y/%m/%d')
 
-find "$start/$today" "$start/$yesterday" -name *.cube.fits 2>/dev/null \
+find "$path/$day" "$path/$day_before" -name *.cube.fits 2>/dev/null \
 | cut -d'/' -f-5 | uniq | while read day
 do
   find "$day" -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$day/cube.json";
