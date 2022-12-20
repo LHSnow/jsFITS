2c888f74e4c37b3e6f058f7f1970e569f9c8fcdb
Lars-Henrik Snow <lars-henrik.snow@irf.se>
2022-12-20 12:09:28 +0100

    defaulting to yesterday + day after to match other ALIS data scripts

--- find-alis-cubes.sh
+++ find-alis-cubes.sh
@@ -1,9 +1,9 @@
 path='/alis/data'
-radix=${1:-today}
+radix=${1:-yesterday}
 day=$(date -d "$radix" '+%Y/%m/%d')
-day_before=$(date -d "$radix -1 day" '+%Y/%m/%d')
+day_after=$(date -d "$radix +1 day" '+%Y/%m/%d')
 
-find "$path/$day" "$path/$day_before" -name *.cube.fits 2>/dev/null \
+find "$path/$day" "$path/$day_after" -name *.cube.fits 2>/dev/null \
 | cut -d'/' -f1-6 | uniq | while read d
 do
   find "$d" -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$d/cube.json";
