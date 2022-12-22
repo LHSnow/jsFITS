359168b4cd2edada5015e8e01d47968a29e808ec
Lars-Henrik Snow <lars-henrik.snow@irf.se>
2022-12-22 11:33:53 +0100

    cube.json should contain relative paths (future: change logic in keogram.html)

--- find-alis-cubes.sh
+++ find-alis-cubes.sh
@@ -6,5 +6,6 @@ day_after=$(date -d "$radix +1 day" '+%Y/%m/%d')
 find "$path/$day" "$path/$day_after" -name *.cube.fits 2>/dev/null \
 | cut -d'/' -f1-6 | uniq | while read d
 do
-  find "$d" -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$d/cube.json";
+  cd "$d"
+  find . -name *.cube.fits | jq -Rs 'split("\n")[:-1]' > "$d/cube.json";
 done
